fa12351dd7cc9fa7df1a0275b889bfcd8d183cdc
casey6624 <caseyrsmith43@gmail.com>
2020-04-03 20:53:13 +0100

    Logo image added with inside the header component

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'products/create'
+  get 'books/create'
   get 'static_pages/home'
   root "static_pages#home"
   get 'static_pages/aboutus'
   get 'static_pages/contactus'
   get 'static_pages/products'
+
+  resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
